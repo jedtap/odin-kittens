992a86ea31fdd2e39da51b172e53a3241f0ddee7
Jed-Tapiador <doom.slaver2@gmail.com>
2022-04-30 21:06:54 +0800

    HTML full done

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,57 @@
+class KittensController < ApplicationController
+
+  def index
+    @kittens = Kitten.all
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+  
+  def new
+    @kitten = Kitten.new
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save
+      flash[:notice] = "New cat saved!"
+      redirect_to @kitten
+    else
+      flash[:alert] = "Meow meow, thats not correct"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @kitten = Kitten.find(params[:id])
+
+    if @kitten.update(kitten_params)
+      flash[:notice] = "New cat updatedated!"
+      redirect_to @kitten
+    else
+      flash[:alert] = "Meow meow, no updates for you"
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  
+  def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
+
+    flash[:notice] = "Cat goes bye bye"
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+
+  def kitten_params
+    params.required(:kitten).permit(:name, :age, :cuteness, :softness)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root "kittens#index"
+
+  resources :kittens
 end
